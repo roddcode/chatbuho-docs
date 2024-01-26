747e89c6c709a4a0e0f94063d3d3927c7d8c532e
Alejandro Alvarado <devale.alvarado@gmail.com>
2024-01-26 12:07:51 -0500

    Update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Documentación Chat Buho',
+  title: 'Roddcode',
   tagline: 'Guia oficial completa y definitiva de Chat Buho',
   favicon: 'img/favicon.ico',
 
