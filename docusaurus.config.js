9ec01ab87dce4cf99d14a4418f76e94f36b28d82
Alejandro Alvarado <devale.alvarado@gmail.com>
2024-01-26 19:25:26 -0500

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -57,7 +57,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/card-img.jpg',
+      image: 'img/docusaurus-social-card.jpg',
       navbar: {
         title: '',
         logo: {
